7619406bd38c795194807f905cd61f879f941b33
Lambru99 <ludovico.latini@studenti.unicam.it>
2020-11-17 18:34:39 +0100

    Crezione scheletro classi
    creazione classi Commerciante e sua interfaccia, interfaccia prodotto e pacco.

---
+++ C3-TeamPAPC/src/main/java/it/cs/unicam/ids/c3/Pacco.java
@@ -0,0 +1,33 @@
+package it.cs.unicam.ids.c3;
+
+
+public class Pacco {
+    private long numero;
+    private String destinazione;
+    private String emittente;
+
+    public long getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public String getDestinazione() {
+        return this.destinazione;
+    }
+
+    public void setDestinazione(String destinazione) {
+        this.destinazione = destinazione;
+    }
+
+    public String getEmittente() {
+        return this.emittente;
+    }
+
+    public void setEmittente(String emittente) {
+        this.emittente = emittente;
+    }
+    
+}

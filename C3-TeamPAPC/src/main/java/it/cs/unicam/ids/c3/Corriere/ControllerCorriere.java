5642dfafac8755aa6caf86a5ef7220914e408046
Ludovico Latini <ludovico.latini@studenti.unicam.it>
2021-01-08 10:25:11 +0100

    Modifica Ordine

--- C3-TeamPAPC/src/main/java/it/cs/unicam/ids/c3/Corriere/ControllerCorriere.java
+++ C3-TeamPAPC/src/main/java/it/cs/unicam/ids/c3/Corriere/ControllerCorriere.java
@@ -5,7 +5,7 @@ import it.cs.unicam.ids.c3.chiamata.ControllerChiamata;
 
 public class ControllerCorriere {
     private ControllerChiamata cChiamata;
-    
+
     public void visuallizzaListaChiamate(){cChiamata.visualizzaChiamate();}
     public void scegliChiamata(long index){cChiamata.scegliChiamata(index);}
     public void cambiaDisponibilita(@NotNull Corriere corriere){corriere.setDisposizione();}
--- C3-TeamPAPC/src/main/java/it/cs/unicam/ids/c3/Prodotto.java
+++ C3-TeamPAPC/src/main/java/it/cs/unicam/ids/c3/Prodotto.java
@@ -2,6 +2,7 @@ package it.cs.unicam.ids.c3;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Random;
 
 public class Prodotto implements ProdottoInterface{
     //in teoria questo id sarà creato automaticamente nel db
@@ -10,14 +11,28 @@ public class Prodotto implements ProdottoInterface{
     private String nome;
     private String descrizione;
     private double prezzo;
+    private long serialCode;
     
-    public Prodotto(){}
+    public Prodotto(){
+        this.serialCode= new Random().nextInt(999999999);
+    }
+
 
     public Prodotto(String nome, String descrizione,double prezzo){
-        super();
+        this();
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
+    }
+
+    @Override
+    public long getSerialCode() {
+        return serialCode;
+    }
+
+    @Override
+    public void setSerialCode(long serialCode) {
+        this.serialCode = serialCode;
     }
 
     @Override
